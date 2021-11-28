105caa9537003b3a8294d7bc73ae7cf25dc56aca
Victor Vermeulen <vcvermeulen@gmail.com>
2021-11-28 14:27:25 +0100

    finish components

--- src/index.js
+++ src/index.js
@@ -2,13 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
 	return (
-		<div className="ui container comments">
-			<CommentDetail author="Victor" time="6pm" comment="Nice post" />
-			<CommentDetail author="Nienke" time="4pm" comment="Great post" />
-			<CommentDetail author="Test" time="1pm" comment="Shit post" />
+		<div className="ui container comments ">
+			<ApprovalCard>
+				<div>
+					<h4>Warning</h4>
+					Are you sure you want to do this?
+				</div>
+			</ApprovalCard>
+			<ApprovalCard>
+				<CommentDetail author="Victor" time="6pm" comment="Nice post" />
+			</ApprovalCard>
+			<ApprovalCard>
+				<CommentDetail author="Nienke" time="4pm" comment="Great post" />
+			</ApprovalCard>
+			<ApprovalCard>
+				<CommentDetail author="Test" time="1pm" comment="Bad post" />
+			</ApprovalCard>
 		</div>
 	);
 };
